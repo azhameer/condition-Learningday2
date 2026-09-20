ac523e42f6ae9ae7e1c42f929928d384861b3df8
Zameer <azhameer13@gmail.com>
2026-09-20 14:48:25 +0700

    Add examcheck.c for grading input values

---
+++ examcheck.c
@@ -0,0 +1,17 @@
+#include <stdio.h>      
+
+int main() {
+    int a;
+    printf("Masukkan nilai a: ");
+    scanf("%d", &a);
+
+   if (a > 90) {
+        printf("Nilai Bagus\n");
+    } else if (a >= 75) {
+        printf("Nilai Cukup\n");
+    } else {
+        printf("Nilai Kurang\n");
+    }
+    
+    return 0;
+}
